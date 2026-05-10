9671fb56d0eec82c5d9a515984da55b358604cb0
HaoRonald <haovt234@gmail.com>
2026-05-10 21:23:26 +0700

    add folder helpers for be, fe and libs for fe

---
+++ client/src/lib/axios.js
@@ -0,0 +1,25 @@
+// client/src/lib/axios.js
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Thêm token vào header nếu có
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
